a9722d271120d37331f6ed0cbb20aa413b37b3a8
Chara <joannahailey@mail.ru>
2019-08-13 16:41:10 +0300

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -9,6 +9,11 @@ gem 'minitest', group: :test
 gem 'minitest-reporters', group: :test
 gem 'rails-controller-testing'
 gem 'bcrypt',         '3.1.12'
+
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+gem 'rails-ujs', '~> 0.1.0'
+
+
 gem 'bootstrap-sass', '3.3.7'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'static_pages#home'
+root 'static_pages#home'
 get  '/help', to: 'static_pages#help'
 get  '/about', to: 'static_pages#about'
 get  '/contacts', to: 'static_pages#contacts'
 get '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get    '/login',   to: 'sessions#new'
+post   '/login',   to: 'sessions#create'
+delete '/logout',  to: 'sessions#destroy'
+
  resources :users
 
 end
