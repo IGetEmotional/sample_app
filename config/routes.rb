02e8fd6199cbbd6dd71102ded9f95cf46e05a017
Chara <joannahailey@mail.ru>
2019-08-13 15:26:57 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ get  '/help', to: 'static_pages#help'
 get  '/about', to: 'static_pages#about'
 get  '/contacts', to: 'static_pages#contacts'
 get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :users
 
 end
 
